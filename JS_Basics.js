e626711819370e3ae777a9ccf4d7aa6bdf1077e3
Swapnil Tiwari <swaptiwari@outlook.com>
2022-06-27 21:41:54 +0530

    JS basics init

---
+++ JS_Basics.js
@@ -0,0 +1,77 @@
+//JavaScript Single-Line Practice
+/*
+  JavaScript Multi-line Practice
+*/
+
+var myName; //Variable declaration
+
+//Storing values with assignment operator
+var a;
+a = 7;
+
+//Assigning one value to another variable
+var a;
+a = 7;
+var b;
+b = a;
+
+//Initializing variables with Assignment operator
+var a = 9;
+
+//Declare string variables
+var myFirstName = "Swapnil";
+var myLastName = "Tiwari";
+
+//using let
+let catName = "Oliver";
+let catSound = "Meow!";
+
+//using let and const
+const FCC = "freeCodeCamp"; // can't change this later. usually const variables are written in all uppercase.
+let fact = "is cool!"; // can change this 
+fact = "is awesome!";
+console.log(FCC, fact);
+
+//Adding 2 numbers with JS
+const sum = 10 + 10;
+
+//Subtracting one from another
+const difference = 45 - 33;
+
+//Multiplying 2 numbers
+const product = 8 * 10;
+
+//Dividing one number by another
+const quotient = 66 / 33;
+
+//Increment operator
+let myVar = 87;
+myVar = ++myVar;
+
+//Decrement operator
+let myVar1 = 11;
+myVar = --myVar;
+
+//Decimal
+const ourDecimal = 5.7;
+let myDecimal = 2.22;
+//Decimals can have all the operations like add, sub, mult, div
+
+//Remainder Operator
+const remainder = 11 % 3;
+
+//Compound Assignment with Augmented Addition, Subtraction, Multiplication, Division
+let a = 3;
+a += 12;
+a -= 2;
+a *= 4;
+a /= 2;
+
+//Escaping Literal Quotes in Strings
+const myStr = "I am a \"double quoted\" string inside \"double quotes\".";
+
+//Using "" and '' together
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+
+
