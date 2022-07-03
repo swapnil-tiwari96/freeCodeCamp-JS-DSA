e09634f5abb9a84be0102db97de05d714976cad0
Swapnil Tiwari <swaptiwari@outlook.com>
2022-07-04 03:51:29 +0530

    Create Debugging.js

---
+++ Debugging.js
@@ -0,0 +1,27 @@
+// Use JS console to check the value of a variable
+let a = 25;
+console.log(a);
+
+// console has few more methods than log like .clear .warn etc
+let output = "Get this to show once in the freeCodeCamp console and not at all in the browser console";
+console.log(output);
+console.clear();
+
+// Use typeof to check the type of the variable
+// JS recognizes 7 primitive (immutable) Data types: Boolean, Null, Undefined, Number, String, Symbol(ES6) and BigInt(2020)
+let seven = 7;
+let three = "3";
+console.log(seven + three);
+console.log(typeof (seven));
+console.log(typeof (three));
+
+// Catch misspelled variables and function names using console.log() and typeof
+// Catch unclosed parantheses, brakets, braces and quotes
+// Catch mixed usage of '' and ""
+// Catch use of assigmnet operator instead of equality operator
+// Catch use of () after a function call
+// Catch arguments passed in the wrong order when calling a function
+// Catch off by one errors when using indexing
+// Use caution when reinitiating variables inside a loop.
+// Prevent infinite loops with a valid termination condition
+
